809c8704df8ea54d79ed3bfbdd16aeab4c636909
ritika314 <ritikadeogharia87@gmail.com>
2026-06-11 17:54:44 +0530

    loops

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Loops from "@/components/homeComponents/Loops";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>home page</>
+    <>
+    <Loops/>
+    </>
   );
 }
