03ae73b54b6b033a0648e27461e8a7be84abbfd0
ritika314 <ritikadeogharia87@gmail.com>
2026-06-12 18:03:28 +0530

    create user

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import CreateUser from "@/components/homeComponents/CreateUser";
 import Loops from "@/components/homeComponents/Loops";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <>
+    <CreateUser />
+    <hr className="my-20" />
+  
     <Loops/>
     </>
   );
